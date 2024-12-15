5d72897740218a63933f9f8dcc1759b611565343
rayhanalmim <rayhanalmim1@gmail.com>
2024-12-15 12:01:15 +0600

    updated code

--- src/utilities/knownTokens.js
+++ src/utilities/knownTokens.js
@@ -1,4 +1,5 @@
 export const knownTokens = [
     { name: "LINK", address: "0x779877A7B0D9E8603169DdbD7836e478b4624789" },
     { name: "WETH", address: "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14" },
+    { name: "ANTO", address: "0x8e5F6C5B4B9B8C65d002c1D5c3429A0A8aB3F3cE" },
   ];
